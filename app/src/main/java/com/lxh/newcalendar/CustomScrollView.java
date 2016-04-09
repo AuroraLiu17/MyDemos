754f9618b9d3af92fff6a447c8a8d5b8e1d659c4
AuroraLiu <1473375730@qq.com>
2016-04-09 21:25:12 +0800

    shit

--- app/src/main/java/com/lxh/newcalendar/CustomScrollView.java
+++ app/src/main/java/com/lxh/newcalendar/CustomScrollView.java
@@ -1,17 +1,28 @@
 package com.lxh.newcalendar;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
 import android.widget.Scroller;
 
 /**
  * Created by liuxiaohui on 4/8/16.
  */
-public class CustomScrollView extends View {
+public class CustomScrollView extends ImageView implements GestureDetector.OnGestureListener {
 
     private Scroller mScroller;
+    private GestureDetector mGestureDetector;
+
+    private static final int[] mColors = new int[] {
+        Color.RED, Color.YELLOW, Color.GREEN, Color.BLUE, Color.BLACK};
+    private int mCurrentColor = 0;
 
     public CustomScrollView(Context context) {
         this(context, null);
@@ -24,6 +35,74 @@ public class CustomScrollView extends View {
     public CustomScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mScroller = new Scroller(context, new AccelerateDecelerateInterpolator());
+        mGestureDetector = new GestureDetector(context, this);
+        updateBG();
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction() & MotionEvent.ACTION_MASK;
+                if (action == MotionEvent.ACTION_UP ||
+                        action == MotionEvent.ACTION_CANCEL) {
+                    updateElevation(5);
+                }
+                return mGestureDetector.onTouchEvent(event);
+            }
+        });
+    }
+//
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        return mGestureDetector.onTouchEvent(event);
+//    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        mCurrentColor = (mCurrentColor + 1) % mColors.length;
+        updateBG();
+        scrollTo((mCurrentColor + 1) % mColors.length  * 100, (mCurrentColor + 1) % mColors.length * 50);
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        updateElevation(10);
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        smoothScrollBy(-(int)distanceX, -(int)distanceY);
+
+        Log.e("GD", "onScroll:" + -(int)distanceX +" ," + -(int)distanceY);
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.e("GD", "onFling:" + velocityX +" ," + velocityY);
+        mScroller.fling(mScroller.getFinalX(), mScroller.getFinalY(), (int)velocityX, (int)velocityY,
+                0, 300, 0, 600);
+        postInvalidate();
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        updateElevation(20);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return true;
+    }
+
+    private void updateElevation(int elevation) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setElevation(elevation);
+        }
+    }
+
+    private void updateBG() {
+        setBackgroundColor(mColors[mCurrentColor]);
     }
 
     //调用此方法滚动到目标位置
@@ -40,15 +119,25 @@ public class CustomScrollView extends View {
         invalidate();//这里必须调用invalidate()才能保证computeScroll()会被调用，否则不一定会刷新界面，看不到滚动效果
     }
 
-    @Override
-    public void computeScroll() {
-        if (mScroller.computeScrollOffset()) {
-            //这里调用View的scrollTo()完成实际的滚动
-            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+//    @Override
+//    public void computeScroll() {
+//        if (mScroller.computeScrollOffset()) {
+//            //这里调用View的scrollTo()完成实际的滚动
+//            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+//            Log.e("GD", "scrollTo:" + mScroller.getCurrX() +" ," + mScroller.getCurrY()
+//                    +" ," + getLeft() +" ," + getTop());
+////            Log.e("GD", "offsetTop:" + (mScroller.getCurrY() - getTop()));
+////            offsetTopAndBottom(mScroller.getCurrY() - getTop());
+////            Log.e("GD", "offsetLeft:" + (mScroller.getCurrX() - getLeft()));
+////            offsetLeftAndRight(mScroller.getCurrX() - getLeft());
+//            //必须调用该方法，否则不一定能看到滚动效果
+//            postInvalidate();
+//        }
+//        super.computeScroll();
+//    }
 
-            //必须调用该方法，否则不一定能看到滚动效果
-            postInvalidate();
-        }
-        super.computeScroll();
+    @Override
+    public void scrollTo(int x, int y) {
+        super.scrollTo(x, y);
     }
 }
--- app/src/main/java/com/lxh/newcalendar/ScrollActivity.java
+++ app/src/main/java/com/lxh/newcalendar/ScrollActivity.java
@@ -1,16 +1,39 @@
 package com.lxh.newcalendar;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by liuxiaohui on 4/8/16.
  */
 public class ScrollActivity extends Activity {
+    private ImageView mImageView;
+    private CustomScrollView mCustomView;
+    private CalendarLifterView mLifterView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll);
+        mImageView = (ImageView) findViewById(R.id.image);
+        mCustomView = (CustomScrollView) findViewById(R.id.custom_view);
+        mLifterView = (CalendarLifterView) findViewById(R.id.lifter_view);
+    }
 
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.up_btn:
+                mImageView.scrollBy(0, -20);
+                mCustomView.scrollBy(0, 20);
+                mLifterView.scrollBy(0, 20);
+                break;
+            case R.id.down_btn:
+                mImageView.scrollBy(0, 20);
+                mCustomView.scrollBy(0, -20);
+                mLifterView.scrollBy(0, -20);
+                break;
+        }
     }
 }
